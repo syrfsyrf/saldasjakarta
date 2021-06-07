1b6958c11c1e3c835334f8b5a82a579f3cd8b042
ferialfahlevi <muhammad.ferial7@gmail.com>
2021-06-07 17:12:08 +0700

    Fixing menu

--- application/models/m_menu.php
+++ application/models/m_menu.php
@@ -11,7 +11,7 @@ class m_menu extends CI_Model {
 
 	public function GetMenuChild()
 	{
-		$hasil = $this->db->query("SELECT id as 'ID2', nama_menu as 'NAMA_MENU2', url as 'URL2', is_parent as 'IS_PARENT2', parent as 'PARENT2', menu as 'MENU2', child as 'CHILD', menu_text as 'MENU_TEXT', icon as 'ICON2' FROM tb_menu WHERE role LIKE '%".$_SESSION['logged_in']['role']."%' AND is_parent IS NULL ORDER BY urutan ASC");
+		$hasil = $this->db->query("SELECT id as 'id2', nama_menu as 'nama_menu2', url as 'url2', is_parent as 'is_parent2', parent as 'parent2', menu as 'menu2', child as 'child', menu_text as 'menu_text', icon as 'icon2' FROM tb_menu WHERE role LIKE '%".$_SESSION['logged_in']['role']."%' AND is_parent IS NULL ORDER BY urutan ASC");
 		return $hasil;
 	}
 }
